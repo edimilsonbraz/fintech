f86647bc220cabd15b461460ef463075ad041af0
edimilson braz <edimilson.gt8@gmail.com>
2023-12-07 15:24:36 -0300

    :sparkles: add pages e Hook useFech

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { Header } from './Components/Header'
+import { Sidenav } from './Components/Sidenav'
+import { Resumo } from './Pages/Resumo'
 import './Style.css'
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <Sidenav />
+      <main>
+        <Header />
+        <Resumo />
+      </main>
+    </div>
   )
 }
 
