22cb960599dc3b3dac241ec4e489b667bb465cb2
edimilson braz <edimilson.gt8@gmail.com>
2023-12-09 09:03:54 -0300

    :sparkles: Add funcion MesBtn

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
 import { DateRange } from './DateRange'
+import { Meses } from './Meses'
 
 export const Header = () => {
-
   return (
-    <div>
-      <DateRange />
-    </div>
+    <header className='mb'>
+      <div className='mb'>
+        <DateRange />
+      </div>
+      <Meses />
+    </header>
   )
 }
