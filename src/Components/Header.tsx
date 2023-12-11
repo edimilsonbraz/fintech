4618e376ff0f0b83165139c216a0c4d2bbf87443
edimilson braz <edimilson.gt8@gmail.com>
2023-12-11 09:01:31 -0300

    :sparkles: Add Routes with Router-dom

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from 'react'
 import { DateRange } from './DateRange'
 import { Meses } from './Meses'
+import { useLocation } from 'react-router-dom'
 
 export const Header = () => {
+  const [title, setTitle] = useState('Resumo')
+  const location = useLocation()
+
+  useEffect(() => {
+    if(location.pathname === '/') {
+      setTitle('Resumo')
+      document.title = "Fintech | Resumo"
+    } else if (location.pathname === '/vendas') {
+      setTitle('Vendas')
+      document.title = "Fintech | Vendas"
+
+    }
+  }, [location])
+
   return (
     <header className='mb'>
-      <div className='mb'>
+      <div className='daterange mb'>
         <DateRange />
+        <h1 className='box bg-3'>{title}</h1>
       </div>
       <Meses />
     </header>
--- src/Components/Sidenav.tsx
+++ src/Components/Sidenav.tsx
@@ -1,7 +1,56 @@
 import React from 'react'
 
+import resumo from '../assets/icons/resumo.svg'
+import vendas from '../assets/icons/vendas.svg'
+import webhooks from '../assets/icons/webhooks.svg'
+import configuracoes from '../assets/icons/configuracoes.svg'
+import contato from '../assets/icons/contato.svg'
+import sair from '../assets/icons/sair.svg'
+import FintechSVG from '../assets/FintechSVG'
+import { NavLink } from 'react-router-dom'
+
 export const Sidenav = () => {
   return (
-    <div>Sidenav</div>
+    <nav className='sidenav box bg-3'>
+      <FintechSVG title='fintech logo'/>
+      <ul>
+        <li>
+          <span>
+            <img src={resumo} alt="resumo" />
+          </span>
+          <NavLink to="/">Resumo</NavLink>
+        </li>
+        <li>
+          <span>
+            <img src={vendas} alt="vendas" />
+          </span>
+          <NavLink to="/vendas">Vendas</NavLink>
+        </li>
+        <li>
+          <span>
+            <img src={webhooks} alt="vendas" />
+          </span>
+          <a href="">Webhooks</a>
+        </li>
+        <li>
+          <span>
+            <img src={configuracoes} alt="vendas" />
+          </span>
+          <a href="#">Configurações</a>
+        </li>
+        <li>
+          <span>
+            <img src={contato} alt="vendas" />
+          </span>
+          <a href="#">Contato</a>
+        </li>
+        <li>
+          <span>
+            <img src={sair} alt="vendas" />
+          </span>
+          <a href="#">Sair</a>
+        </li>
+      </ul>
+    </nav>
   )
 }
